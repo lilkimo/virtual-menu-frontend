0d788be9e923907082459cf455890ec4c28a21bb
Zarko Kuljis <zarkokuljis.zk@gmail.com>
2023-09-10 17:41:49 -0300

    tags improvements

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -7,11 +7,19 @@ import menu from "../assets/carta.json";
 
 const route = useRoute();
 const data = ref(menu.restaurants[0]); //api.get(`/${route.params.id}`))
+
+const tags = {
+  'vegano': 'text-[#17ad17] border-[#17ad17] bg-[#eefdee]',
+  'vegetariano': 'text-[#17ad17] border-[#17ad17] bg-[#eefdee]',//['#32cd32', '#c0f0c0'],
+  'camarón': 'text-[#ff8989] border-[#ff8989] bg-[#fdeded]',//['#32cd32', '#c0f0c0'],
+  'sin gluten': 'text-[#f8c52b] border-[#f8c52b] bg-[#fef9e9]',//['#32cd32', '#c0f0c0'],
+  'sin lactosa': 'text-[#6039da] border-[#6039da] bg-[#f2eefc]',//['#32cd32', '#c0f0c0'],
+}
 </script>
 
 <template>
   <div>
-    <div class="grid h-24 grid-cols-2 bg-[#A73121]">
+    <div class="grid h-24 grid-cols-2 bg-[#79f]">
       <div>
         <img src="../assets/sushi.png" />
       </div>
@@ -21,29 +29,32 @@ const data = ref(menu.restaurants[0]); //api.get(`/${route.params.id}`))
       </div>
     </div>
 
-    <div class="bg-[#fcf7f1] p-6">
+    <div class="bg-background p-2">
       <span
         v-for="dish in data.menu"
-        class="mb-3 grid grid-cols-12 rounded-lg bg-[#fcf7f1] p-3 drop-shadow-lg"
+        class="mb-2 grid grid-cols-12 rounded-lg bg-background p-2 drop-shadow-lg"
       >
         <img class="col-start-1 col-end-5" src="../assets/Nigiri Sake.png" />
         <div class="col-start-5 col-end-13 flex flex-col">
-          <span class="text-left text-xl font-bold text-[#862B0D]">
+          <span class="text-left text-xl font-bold text-text">
             {{ dish.name }}
           </span>
-          <span class="text-left leading-4 text-[#C56545]">
+          <span class="text-left leading-4 text-text h-full">
             {{ dish.description }}
           </span>
-          <span class="flex flex-row-reverse justify-between">
-            <span class="text-left text-xl font-bold text-[#862B0D]">
+          <span class="flex flex-row-reverse gap-1 justify-between">
+            <span class="text-left text-xl font-bold text-text">
               ${{ dish.price }}
             </span>
-            <span
-              v-for="tag in dish.tags"
-              class="my-1 place-self-center rounded-full border-2 border-solid border-[#862B0D] px-2 text-xs lowercase leading-3 text-[#862B0D]"
-            >
-              {{ tag }}
-            </span>
+            <div class="inline-flex gap-1 overflow-auto">
+              <span
+                v-for="tag in dish.tags"
+                class="py-[.125rem] place-self-center rounded-full border-2 border-solid px-[.375rem] text-xs leading-3 font-PTSansNarrow uppercase whitespace-nowrap"
+                :class="tags[tag]"
+              >
+                {{ tag }}
+              </span>
+            </div>
           </span>
         </div>
       </span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx,vue,html}'
   ],
   theme: {
+    colors: {
+      'background': '#ffffff',
+      'text': '#000000',
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      PTSansNarrow: ['PT Sans Narrow', 'sans-serif'],
+      // yanone: ['Yanone Kaffeesatz', 'sans-serif'],
+      // patuaOne: ['Patua One', 'cursive'],
+      // antonio: ['Antonio', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
