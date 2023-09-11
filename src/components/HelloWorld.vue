53d965681d74668a81b0e170cc7df47a97543661
L0nkk <aiday33@hotmail.com>
2023-09-11 15:04:14 -0300

    dispach

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,62 +1,68 @@
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
 
-const count = ref(0);
+import api from '../api'
+import menu from '../assets/carta.json'
+
+
+const route = useRoute()
+const data = ref(menu.restaurants[0])//api.get(`/${route.params.id}`))
 </script>
 
 <template>
+
   <h1
-    class="bg-gradient-to-r from-green-600 to-sky-400 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent"
+    class="bg-clip-text text-2xl font-black"
   >
-    Vite + Vue 3 + Tailwindcss v3
+    <span
+  style="
+    display: block; 
+    max-width: 90%; 
+    border: 1px solid gray; 
+    margin: 10px auto 10px;
+    padding: 10px;
+    line-height: 1.5; 
+    border-radius: 10px;
+  "
+>
+   <span style="color: rgb(23, 23, 23);">id orden:</span> <br>
+  nombre: <br>
+  numero: <br>
+  dirrecion: <br>
+</span>
+
+    <span
+    v-for="dish in data.menu" 
+    style="
+      display: block; 
+      max-width: 90%; 
+      border: 1px solid gray; 
+      margin: 0 auto 10px;
+      padding: 10px;
+      line-height: 1; 
+      text-align: center;
+      border-radius: 10px;
+      "
+    > 
+
+    <h1 style="font-size: 90%; color:rgb(34, 34, 34); text-align: left;">{{ dish.name }}</h1>
+    <table style="width: 100%;">
+    <tr>
+      <td style="width: 20%; ;">
+        <img src="https://www.yamakisushi.cl/wp-content/uploads/2020/07/VR4205.png" style="border-radius: 20px;">
+      </td>
+      <td style="text-align: left; vertical-align: top;">
+        <h1>comentarios</h1>
+      </td>
+    </tr>
+  </table>
+  <h1 style="vertical-align: bottom; font-size: 90%; color:rgb(34, 34, 34); text-align: left;">Cantidad: 1</h1>
+
+</span>
   </h1>
-
-  <p class="mt-6 space-x-4">
-    Recommended IDE setup:
-    <a
-      href="https://code.visualstudio.com/"
-      class="text-emerald-500 underline transition-all hover:text-emerald-400"
-      target="_blank"
-      >VSCode</a
-    >
-    +
-    <a
-      href="https://github.com/johnsoncodehk/volar"
-      class="text-emerald-500 underline transition-all hover:text-emerald-400"
-      target="_blank"
-      >Volar</a
-    >
-  </p>
-
-  <p class="mt-4 space-x-4">
-    <a
-      href="https://vitejs.dev/guide/features.html"
-      class="text-emerald-500 underline transition-all hover:text-emerald-400"
-      target="_blank"
-    >
-      Vite Documentation
-    </a>
-    |
-    <a
-      href="https://v3.vuejs.org/"
-      class="text-emerald-500 underline transition-all hover:text-emerald-400"
-      target="_blank"
-      >Vue 3 Documentation</a
-    >
-  </p>
-
-  <button
-    type="button"
-    class="my-6 rounded bg-gray-300 px-4 py-2 transition-all hover:bg-gray-200"
-    @click="count++"
-  >
-    count is: {{ count }}
-  </button>
-  <p>
-    Edit
-    <code class="text-gray-400">components/HelloWorld.vue</code> to test hot
-    module replacement.
-  </p>
 </template>
 
-<style scoped></style>
+
+<style scoped>
+</style>
